60498fdb72a310235ed18a895ae853068d02fb57
Karl Westgårdh <karl.westgardh@gmail.com>
2018-11-29 16:43:06 +0100

    Created currentDate file

---
+++ MiniProject/src/Main/CurrentDate.java
@@ -0,0 +1,12 @@
+package Main;
+
+import java.time.LocalDate;
+
+public class CurrentDate {
+
+    public LocalDate getCurrentDate(){
+        LocalDate currentDate = LocalDate.now();
+        return currentDate;
+    }
+}
+
