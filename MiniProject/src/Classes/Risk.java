868df803691dd670528c305a6ceb19d156a5a7ee
nafenk <gushajana@student.gu.se>
2018-11-29 12:54:09 +0100

    risk

--- MiniProject/src/Classes/Risk.java
+++ MiniProject/src/Classes/Risk.java
@@ -1,4 +1,32 @@
 package Classes;
 
 public class Risk {
+    private int riskId;
+    private String riskName;
+    private double probability;
+    private int impact;
+
+    public Risk( int riskId, String riskname, double probability, int impact){
+        this.riskId= riskId;
+        this.riskName= riskname;
+        this.probability= probability;
+        this.impact= impact;
+
+    }
+
+    public int getRiskId(){
+        return this.riskId;
+    }
+    public String getRiskName(){
+        return this.riskName;
+    }
+    public double getProbability() {
+        return this.probability;
+    }
+    public int getImpact(){
+        return this.impact;
+    }
 }
+
+
+
