4ea47692ef3456337ca2496f5b748fa285307049
Karl Westgårdh <karl.westgardh@gmail.com>
2018-12-14 15:03:01 +0100

    added method to project to calculated distance between start of project and start of task

--- MiniProject/src/Main/TestMain.java
+++ MiniProject/src/Main/TestMain.java
@@ -1,7 +1,6 @@
 package Main;
-/*
+
 import Classes.*;
-import Output.Output;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -13,15 +12,17 @@ public class TestMain {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        //1 - Creating a new project
+        //1 - Creating a new project and printing the object
 
         String strA1 = "2018-10-01";
         LocalDate dateA1 = LocalDate.parse(strA1,formatter);
-        String strB1 = "2018-11-01";
+        String strB1 = "2018-11-30";
         LocalDate dateA2 = LocalDate.parse(strB1,formatter);
 
         Project project1 = new Project(1,"Duckies",dateA1,dateA2,
-                430000);
+                100000);
+
+        System.out.println(project1);
 
         //2 - Creating 3 new risks & inserting them into the riskList in project1
 
@@ -33,107 +34,111 @@ public class TestMain {
         project1.getRiskList().add(risk2);
         project1.getRiskList().add(risk3);
 
+        System.out.println(project1.getRiskList());
+
         //3 - Creating 3 new teamMembers & inserting them into the teamMemberList in project1
 
         TeamMember teamMember1 = new TeamMember(1,"Nafen",100);
-        TeamMember teamMember2 = new TeamMember(2,"Thiago",350);
-        TeamMember teamMember3 = new TeamMember(3,"Emanuel",300);
+        TeamMember teamMember2 = new TeamMember(2,"Thiago",200);
+        TeamMember teamMember3 = new TeamMember(3,"Emanuel",400);
 
         project1.getTeamMemberList().add(teamMember1);
         project1.getTeamMemberList().add(teamMember2);
         project1.getTeamMemberList().add(teamMember3);
 
-        //4 - Creating 2 new tasks & inserting it into the taskList in project1
+        System.out.println(project1.getTeamMemberList());
 
-        String str1 = "2018-11-02";
+        //4 - Creating 3 new tasks & inserting it into the taskList in project1
+
+        String str1 = "2018-10-01";
         LocalDate date1 = LocalDate.parse(str1,formatter);
-        String str2 = "2019-01-01";
+        String str2 = "2018-10-15";
         LocalDate date2 = LocalDate.parse(str2,formatter);
-        String str3 = "2018-11-11";
+        String str3 = "2018-10-08";
         LocalDate date3 = LocalDate.parse(str3,formatter);
 
-        HashMap<Integer,Double> map1 = new HashMap<Integer,Double>();
+        HashMap<Integer,Double> map1 = new HashMap<>();
         map1.put(1,10.0);
         map1.put(2,10.0);
 
-        Task task1 = new Task(1,"CreateGitRepository","Github 4-life",
+        Task task1 = new Task(1,"Setup Slack","Slack 4-life",
                 date1,date2,date3,map1);
         project1.getTaskList().add(task1);
 
-        String strb1 = "2018-11-02";
-        LocalDate dateb1 = LocalDate.parse(strb1,formatter);
-        String strb2 = "2018-12-02";
-        LocalDate dateb2 = LocalDate.parse(strb2,formatter);
-        String strb3 = "2018-11-30";
-        LocalDate dateb3 = LocalDate.parse(strb3,formatter);
+        String str4 = "2018-10-08";
+        LocalDate date4 = LocalDate.parse(str4,formatter);
+        String str5 = "2018-10-22";
+        LocalDate date5 = LocalDate.parse(str5,formatter);
+        String str6 = "2018-10-15";
+        LocalDate date6 = LocalDate.parse(str6,formatter);
 
-        HashMap<Integer,Double> map2 = new HashMap<Integer,Double>();
-        map2.put(1,15.0);
-        map2.put(2,10.0);
+        HashMap<Integer,Double> map2 = new HashMap<>();
+        map2.put(1,10.0);
+        map2.put(3,10.0);
 
-        Task task2 = new Task(1,"CreateGittyKitty","Github 4-life",
-                dateb1,dateb2,dateb3,map2);
+        Task task2 = new Task(2,"Setup GitHub","Github 4-life",
+                date4,date5,date6,map2);
         project1.getTaskList().add(task2);
 
-        // 5 - testing calculateEV
-        System.out.println(project1.getTaskList());
-        System.out.println(task1.getTaskLength());
-        System.out.println(task2.getTaskLength());
-        String str4 = "2018-11-15";
-        LocalDate date4 = LocalDate.parse(str4,formatter);
-        System.out.println(task1.taskIsComplete(date4));
-        System.out.println(task2.taskIsComplete(date4));
-        System.out.println(project1.percentageOfCompletedTasks(date4));
-        System.out.println(project1.calculateEV(date4));
+        String str7 = "2018-10-15";
+        LocalDate date7 = LocalDate.parse(str7,formatter);
+        String str8 = "2018-10-29";
+        LocalDate date8 = LocalDate.parse(str8,formatter);
+        String str9 = "2018-10-22";
+        LocalDate date9 = LocalDate.parse(str9,formatter);
 
-        //6 - testing calculate SV
-        System.out.println("*****************************");
-        System.out.println(project1.calculateEV(date4));
-        System.out.println(project1.calculatePV(date4));
-        System.out.println(project1.calculateSV(date4));
+        HashMap<Integer,Double> map3 = new HashMap<>();
+        map3.put(2,20.0);
+        map3.put(3,30.0);
 
-        //7 - testing workByMember
-
-        System.out.println(project1.assignedTasksByMember(1));
-
-        //8 - testing retrieveRisks
-
-        System.out.println(project1.retrieveRisks());
-
-        System.out.println(task1.getTaskLength());
-        String tempStr = "2018-11-17";
-        LocalDate tempDate = LocalDate.parse(tempStr,formatter);
-        System.out.println(task1.progressInHours(tempDate));
-
-        System.out.println(project1.calculateAC(tempDate));
-
-        System.out.println(project1.calculateEV(tempDate));
-        System.out.println(project1.calculateAC(tempDate));
-        System.out.println(project1.calculateCV(tempDate));
-
-        System.out.println(project1.returnProjectIntervalDates());
-
-        System.out.println(project1.getTaskList());
-
-        // 9 - testing outputs
-
-        Output output1 = new Output();
-
-        output1.displayRiskMatrix(project1.getRiskList());
-        System.out.println(("***********************"));
-        output1.displayProjectVariance(project1);
-        System.out.println();
+        Task task3 = new Task(3,"Setup Trello","Trello 4-life",
+                date7,date8,date9,map3);
+        project1.getTaskList().add(task3);
 
 
-        //10 - testing result of displayTaskByMember
+        //5 - testing calculateEV
 
-        System.out.println(("***********************"));
-        output1.displayTaskByMember(project1,1);
+        //testing submethod of calculateEV -> percentageOfCompletedTasks
+        String str10 = "2018-10-16";
+        LocalDate date10 = LocalDate.parse(str10,formatter);
+        System.out.println(project1.percentageOfCompletedTasks(date10)); // should return 0.66666666 since two tasks are completed before 2018-10-16 -> 0.6666666
 
-        output1.displayHoursPerTeamMember(project1);
+        //testing submethod of calculateEV -> getBudgetAtCompletion
+        System.out.println(project1.getBudgetAtCompletion()); // should return 100.000 since the project budget is set to 100.000 -> 100000.0
+
+        //testing main method
+
+        System.out.println(project1.calculateEV(date10)); // should return 66.666 since 0.666666 * 100.000 as above -> 66666.66
+
+        //6 - testing calculateSV
+
+        //testing submethod of calculateSV -> calculateEV
+        System.out.println(project1.calculateEV(date10)); //should return 66.666 as in test above -> 66666.66
+
+        //testing submethod of calculateSV -> calculatePV
+        //method will print timeelapsed which is the number of days between startDateOfProject to (checkedDate) -> 15
+        //method will print projectDuration which is the number of days between startDateOfProject to date of completion of project -> 60
+        //method will do calculation -> timeElapsed / projectDuration * budgetAtCompletion which is currently 0.25*100.000 -> 25.000
+
+        System.out.println(project1.calculatePV(date10));
+
+        //testing main method
+
+        System.out.println(project1.calculateSV(date10)); // should return 66666.66666 - 25000.0 -> 41666.66
+
+
+        //7 - testing calculateCV
+
+        //testing submethod of calculateCV -> calculateEV
+        System.out.println(project1.calculateEV(date10)); // should return calculateEV as above -> 66666.66
+
+        //testing submethod of calculateCV -> calculateAC
+        //method will print 20000, 60000, 160000 which is the sum per each teamMember
+        System.out.println(project1.calculateAC(date10)); // should return 240000
+
+        //testing main method
+        System.out.println(project1.calculateCV(date10));
 
     }
 
 }
-*/
-
