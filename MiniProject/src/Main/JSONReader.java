8c75ed83ba31665d40078d5140258c275dd4636e
Thiago Oliva <thiagohtoliva@gmail.com>
2018-12-18 15:29:42 +0100

    new JsonExceptionMethods class with all exceptions for the JSON Reader. Also added comments to the exceptions

--- MiniProject/src/Main/JSONReader.java
+++ MiniProject/src/Main/JSONReader.java
@@ -16,6 +16,8 @@ import java.util.*;
 
 public class JSONReader {
 
+    private JsonExceptionMethods exceptionsClass = new JsonExceptionMethods();
+
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     private JSONObject createJSONObject() throws ParseException, IOException {
@@ -29,50 +31,32 @@ public class JSONReader {
     protected List<Project> createProjects() throws IOException, ParseException {
 
         List<Project> arrayListOfProjects = new ArrayList<>();
-
         JSONArray listOfProjects = (JSONArray) createJSONObject().get("listOfProjects");
-
         Iterator projectIterator = listOfProjects.iterator();
 
         while (projectIterator.hasNext()) {
 
             Map projectMap = (Map) projectIterator.next();
 
-
             //Checks if the projectId is a String
+            exceptionsClass.validateProjectId((CharSequence) projectMap.get("projectId"));
 
-            if (!StringUtils.isNumeric((CharSequence) projectMap.get("projectId"))) {
-              throw new JSONException("Ooops... Looks like your Project ID contains letters. Please enter a number instead");
-            }
-
-//          String regex = "(1[0-2]|0[1-9])/[0-9]{4}$/^(3[01]|[12][0-9]|0[1-9])";
-//          Pattern pattern = Pattern.compile(regex);
-//          Matcher matcher = pattern.matcher(projectedCompletedDate);
-
-            if (!((String) projectMap.get("actualStartDate")).matches("\\d{4}-\\d{2}-\\d{2}")) {
-                throw new JSONException("Oj oj oj... Looks like you have input an invalid data format");
-            }
-
-            if (!((String) projectMap.get("projectedCompletedDate")).matches("\\d{4}-\\d{2}-\\d{2}")) {
-                throw new JSONException("Oj oj oj... Looks like you have input an invalid data format");
-            }
-
+            //Checks if project actualStartDate and projectCompletedDate are in the format yyyy-mm-dd
+            exceptionsClass.checkDate((String) projectMap.get("actualStartDate"));
+            exceptionsClass.checkDate((String) projectMap.get("projectedCompletedDate"));
 
             int projectId = Integer.valueOf((String) projectMap.get("projectId"));
             String projectName = (String) projectMap.get("projectName");
+
             LocalDate actualStartDate = LocalDate.parse((String) projectMap.get("actualStartDate"), formatter);
             LocalDate projectedCompletedDate = LocalDate.parse((String) projectMap.get("projectedCompletedDate"), formatter);
 
-            if (projectedCompletedDate.compareTo(actualStartDate) < 0) {
-                throw new JSONException("Hmmm... Looks like you set your end date before your start date. Please check the JSON-file and try again.");
-            }
+            //checks if start date is before end date
+            exceptionsClass.checkProjectCompletedDateAfterStart(projectedCompletedDate, actualStartDate);
 
             Double budgetAtCompletion = Double.valueOf((String) projectMap.get("budgetAtCompletion"));
-
-            if (budgetAtCompletion <= 0) {
-                throw new JSONException("Ooops.... Looks like your budgetAtCompletion is negative or at zero. Please check your JSON-file and try again");
-            }
-
+            //validates if BudgetAtCompletion is positive
+            exceptionsClass.validateBudgetAtCompletion(budgetAtCompletion);
 
             JSONArray teammates = (JSONArray) projectMap.get("teamMemberList");
             List<TeamMember> tempArrayListOfTeamMember = createTeamMember(teammates);
@@ -82,7 +66,6 @@ public class JSONReader {
 
             JSONArray risks = (JSONArray) projectMap.get("riskList");
             List<Risk> tempArrayListOfRisk = createRisk(risks);
-
 
             Project newProject = new Project(projectId, projectName, actualStartDate, projectedCompletedDate,
                     budgetAtCompletion);
@@ -98,145 +81,103 @@ public class JSONReader {
     private List<TeamMember> createTeamMember(JSONArray teamMemberList) {
 
         List<TeamMember> arrayListOfTeamMembers = new ArrayList<>();
-
         Iterator teamMateIterator = teamMemberList.iterator();
 
         while (teamMateIterator.hasNext()) {
 
             Map member = (Map) teamMateIterator.next();
 
-
-            //Checks if the teamMemberID is a String
-            //if (!StringUtils.isNumeric((CharSequence) member.get("teamMemberId"))) {
-            //  throw new JSONException("Ooops... Looks like one teamMemberID ID contains letters. Please enter a number instead");
-            //}
+            //Checks if the teamMemberID is an int
+            exceptionsClass.validateTeamMemberId((CharSequence) member.get("teamMemberId"));
 
             int teamMemberId = Integer.valueOf((String) member.get("teamMemberId"));
             String teamMemberName = (String) member.get("teamMemberName");
 
             double salaryPerHour = Double.valueOf((String) member.get("salaryPerHour"));
 
-            if (salaryPerHour < 0) {
-                throw new JSONException("Ooops.... Looks like your salaryPerHour is negative. Please check your JSON-file and try again");
-            }
+            //checks if salaryPerHour is negative
+            exceptionsClass.validateSalaryPerHour(salaryPerHour);
 
             TeamMember newTeamMember = new TeamMember(teamMemberId, teamMemberName, salaryPerHour);
-
             arrayListOfTeamMembers.add(newTeamMember);
         }
-
         return arrayListOfTeamMembers;
     }
 
     private List<Task> createTask(JSONArray taskList) throws IOException, ParseException {
 
         List<Task> arrayListOfTasks = new ArrayList<>();
-
         Iterator taskIterator = taskList.iterator();
-
 
         while (taskIterator.hasNext()) {
 
             Map taskMap = (Map) taskIterator.next();
 
-
-            //Checks if the taskID is a String
-
-            if (!StringUtils.isNumeric((CharSequence) taskMap.get("taskId"))) {
-              throw new JSONException("Ooops... Looks like one taskID contains letters. Please enter a number instead");
-            }
+            //Checks if the taskID is an integer
+            exceptionsClass.validateTaskId((CharSequence) taskMap.get("taskId"));
 
             int taskId = Integer.valueOf((String) taskMap.get("taskId"));
 
             String taskName = (String) taskMap.get("taskName");
             String description = (String) taskMap.get("description");
 
-            if (!((String) taskMap.get("actualStartDate")).matches("\\d{4}-\\d{2}-\\d{2}")) {
-                throw new JSONException("Oj oj oj... Looks like you have input an invalid data format");
-            }
-
-            if (!((String) taskMap.get("projectedCompletedDate")).matches("\\d{4}-\\d{2}-\\d{2}")) {
-                throw new JSONException("Oj oj oj... Looks like you have input an invalid data format");
-            }
-
-            if (!((String) taskMap.get("actualCompletedDate")).matches("\\d{4}-\\d{2}-\\d{2}")) {
-                throw new JSONException("Oj oj oj... Looks like you have input an invalid data format");
-            }
+            //validates, with regex, data format yyyy-mm-dd
+            exceptionsClass.checkDate((String) taskMap.get("actualStartDate"));
+            exceptionsClass.checkDate((String) taskMap.get("projectedCompletedDate"));
+            exceptionsClass.checkDate((String) taskMap.get("actualCompletedDate"));
 
             LocalDate actualStartDate = LocalDate.parse((String) taskMap.get("actualStartDate"), formatter);
             LocalDate projectedCompletedDate = LocalDate.parse((String) taskMap.get("projectedCompletedDate"), formatter);
             LocalDate actualCompletedDate = LocalDate.parse((String) taskMap.get("actualCompletedDate"), formatter);
 
-            if (projectedCompletedDate.compareTo(actualStartDate) < 0) {
-                throw new JSONException("Hmmm... Looks like you set your projectedCompletedDate of a task is before your actualStartDate. Please check the JSON-file and try again.");
-            }
-
-            if (actualCompletedDate.compareTo(actualStartDate) < 0) {
-                throw new JSONException("Hmmm... Looks like you set your actualCompletedDate of a task before your start date. Please check the JSON-file and try again.");
-            }
+            //this checks if projectedCompletedDate and actualCompletedDate are after actualStartDate of tasks
+            exceptionsClass.checkTaskEndDatesAfterStart(actualStartDate, projectedCompletedDate, actualCompletedDate);
 
             JSONObject taskMemberArray = (JSONObject) taskMap.get("hoursWorkedPerTeamMembers");
-
             Task newTask = new Task(taskId, taskName, description, actualStartDate, projectedCompletedDate,
                     actualCompletedDate, createTaskMembers(taskMemberArray));
 
             arrayListOfTasks.add(newTask);
-
         }
         return arrayListOfTasks;
-
     }
 
     private List<Risk> createRisk(JSONArray riskList) {
 
         List<Risk> arrayListOfRisks = new ArrayList<>();
-
         Iterator riskIterator = riskList.iterator();
-
         while (riskIterator.hasNext()) {
 
             Map riskMap = (Map) riskIterator.next();
 
             //Checks if the riskID is a String
-            //if (!StringUtils.isNumeric((CharSequence) riskMap.get("riskId"))) {
-            //   throw new JSONException("Ooops... Looks like one riskID contains letters. Please enter a number instead");
-            //}
+            exceptionsClass.validateRiskId((CharSequence) riskMap.get("riskId"));
 
             int riskId = Integer.valueOf((String) riskMap.get("riskId"));
             String riskName = (String) riskMap.get("riskName");
             double probability = Double.valueOf((String) riskMap.get("probability"));
             int impact = Integer.valueOf((String) riskMap.get("impact"));
 
-            if (probability < 0 || impact < 0) {
-                throw new JSONException("Hey there! Don't put a negative impact or probability into your RiskMatrix");
-            }
+            //Checks if probability and impact are not negative
+            exceptionsClass.validateRiskProbabilityAndImpact(probability, impact);
 
             Risk newRisk = new Risk(riskId, riskName, probability, impact);
-
             arrayListOfRisks.add(newRisk);
         }
-
         return arrayListOfRisks;
     }
-
 
     private Map<Integer, Double> createTaskMembers(JSONObject taskList) {
 
         Map<Integer, Double> mapOfTaskMembers = new HashMap<>();
-
-
         Iterator taskMateIterator = taskList.keySet().iterator();
 
         while (taskMateIterator.hasNext()) {
 
             Integer key = Integer.valueOf((String) taskMateIterator.next());
-
             double value = Double.valueOf((String) taskList.get(key.toString()));
-
             mapOfTaskMembers.put(key, value);
-
         }
-
         return mapOfTaskMembers;
     }
 
