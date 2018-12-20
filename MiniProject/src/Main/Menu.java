d1babe31eaba9151075f2699d860feed742fc036
Karl Westgårdh <karl.westgardh@gmail.com>
2018-12-20 23:15:18 +0100

    added validation to input of menu option

--- MiniProject/src/Main/Menu.java
+++ MiniProject/src/Main/Menu.java
@@ -7,6 +7,12 @@ import java.util.Scanner;
 
 public class Menu {
 
+    private final static int PROJECT_SCHEDULE = 1;
+    private final static int PROJECT_VARIANCE = 2;
+    private final static int RISK_MATRIX = 3;
+    private final static int TASKS_BY_MEMBER = 4;
+    private final static int HOURS_BY_MEMBER = 5;
+
     public static final String[] menuOptions = {
             "1. Project Schedule",
             "2. Project Variances",
@@ -23,27 +29,32 @@ public class Menu {
 
         do {
             output.displayMenu(menuOptions);
-            output.displayMessage("Select an option...");
+            while (!sc.hasNextInt()){
+                output.displayMessage("Select an option...");
+                String input = sc.next();
+                output.displayMessage(String.format("\"%s\" is not a valid option.\n",input));
+                output.displayMenu(menuOptions);
+            }
             option = sc.nextInt();
             sc.nextLine();
 
             switch (option) {
-                case 1:
+                case PROJECT_SCHEDULE:
                     output.displayProjectSchedule(project);
                     break;
-                case 2:
+                case PROJECT_VARIANCE:
                     output.displayProjectVariance(project);
                     break;
-                case 3:
+                case RISK_MATRIX:
                     output.displayRiskMatrix(project);
                     break;
-                case 4:
+                case TASKS_BY_MEMBER:
                     output.displayTeamMembers(project);
                     output.displayMessage("Enter team member ID:");
                     int teamMemberId = sc.nextInt();
                     output.displayTaskByMember(project,teamMemberId);
                     break;
-                case 5:
+                case HOURS_BY_MEMBER:
                     output.displayHoursPerTeamMember(project);
                     break;
 
