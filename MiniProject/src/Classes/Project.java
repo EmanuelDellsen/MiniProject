c2abe679d43ee5bebfa2af5369738d9a4e95ac3e
Nafen <gushajana@student.gu.se>
2018-12-26 00:10:16 +0100

    added some comments to the project class--> to be continued

--- MiniProject/src/Classes/Project.java
+++ MiniProject/src/Classes/Project.java
@@ -31,29 +31,52 @@ public class Project {
 
     public Project(){
 
-    }
+    } //TODO: this method is unnecessary
 
     public int getProjectId() {
         return projectId;
     }
+    /*
+    to calculate (EV = eared value), the percentage of the
+    completed tasks is multiplied with the budget of the project at completion.
+     */
 
     public double calculateEV(LocalDate date){
         return (this.percentageOfCompletedTasks(date)*this.budgetAtCompletion);
     }
+    /*
+    to calculate (SV = Scheduled Variance), the difference between
+    earned value (EV) and planned value (PV) is calculated.
+     */
 
     public double calculateSV(LocalDate date){
         return (this.calculateEV(date)-this.calculatePV(date));
     }
+    /*
+    to calculate (CV = cost variance), the difference between
+    earned value (EV) and actual value (AC) is calculated.
+     */
 
     public double calculateCV(LocalDate date){
         return this.calculateEV(date)-this.calculateAC(date);
     }
+    /*
+     Primitive stream that collects the tasks done by a team member
+     by calling the getTaskmembers() method of type map from Task Class
+     which contains the task members.
+     */
+
 
     public List<Task> returnTasksByTeamMember(int teamMemberId){
         return taskList.stream()
                 .filter(task -> task.getTaskMembers().containsKey(teamMemberId))
                 .collect(Collectors.toList());
     }
+    /*
+    stream that operates the sum of hours spent by each team
+    member by calling returnHoursByMember() method from the
+    Task class in for each.
+     */
 
     public double returnHoursByTeamMember(int teamMemberId){
         double sum = 0.0;
@@ -63,20 +86,27 @@ public class Project {
         }
         return sum;
     }
+    /*
+    stream that operates the sum of the total hours spent on the project
+     */
 
     public double returnAllHoursWorked(){
         return this.taskList.stream()
                 .mapToDouble(Task::returnHoursInTask)
                 .sum();
     }
-
+    //TODO: why do we need  that method
     public long returnDaysBetweenProjectAndTask(Task task){
         return ChronoUnit.DAYS.between(this.actualStartDate,task.getActualStartDate());
     }
+    /*
+    to return the number of days the project lasts
+     */
 
     public long returnNumberOfWeeksInProject(){
         return ChronoUnit.WEEKS.between(this.actualStartDate,this.projectedCompletedDate);
     }
+    //TODO we might not really need this method, I will introduce a retrieve teamMember method instead
 
     public TeamMember returnTeamMember(int teamMemberId){
         return this.teamMemberList.stream()
@@ -84,12 +114,22 @@ public class Project {
                 .findAny()
                 .orElse(null);
     }
+    /*
+    stream that collects the list of risks and
+    sorts them in an alphabetical order
+     */
 
     public List<Risk> returnRisks(){
         return this.riskList.stream()
                 .sorted(Comparator.comparing(Risk::returnRisk))
                 .collect(Collectors.toList());
     }
+    /*
+    this method creates a "teamList" or a temporary list which
+    copares the number of hours spent by each team Member and
+    sorts the members from those who spent the most hours to
+    the least number of hours
+     */
 
     public List<TeamMember> returnTeamMembersSortedByHours(){
 
@@ -101,11 +141,17 @@ public class Project {
         return tempList;
     }
 
+
     public List<LocalDate> returnDatesPerInterval(int skipNumOfWeeks){
         return this.actualStartDate.datesUntil(this.projectedCompletedDate,Period.ofWeeks(2))
                 .skip(skipNumOfWeeks)
                 .collect(Collectors.toList());
     }
+    /*
+    this method return the percentage of the completed task,
+    that percentage is used in the above method to calculate the
+    earned value
+     */
 
     private double percentageOfCompletedTasks(LocalDate date){
         double valueOfCompletedTasks = 0.0;
@@ -119,6 +165,9 @@ public class Project {
         }
         return (valueOfCompletedTasks/valueOfAllTasks);
     }
+    /*
+    this method return the planned value for the project
+     */
 
     private double calculatePV(LocalDate date){
         long timeElapsed = ChronoUnit.DAYS.between(this.actualStartDate,date);
@@ -128,6 +177,7 @@ public class Project {
 
         return (percentageOfProjectCompleted*this.budgetAtCompletion);
     }
+
 
     private double calculateAC(LocalDate date){
         double sumOfProgress = 0.0;
